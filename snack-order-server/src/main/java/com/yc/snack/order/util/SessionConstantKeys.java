4bc9ad25b4916e9cdee35cfbea38652b1f2000fc
qiuyuzyy <1421445024@qq.com>
2020-10-04 11:27:20 +0800

    模块拆分

--- snack-order-server/src/main/java/com/yc/snack/order/util/SessionConstantKeys.java
+++
@@ -1,6 +0,0 @@
-package com.yc.snack.order.util;
-
-public class SessionConstantKeys {
-	public static final String CURRENTBACKLOGINACCOUNT = "currentBackLogin"; // 存放后台登录用户信息的
-	public static final String CURRENTMEMBERACCOUNT = "currentMember"; // 存放前台登录的会员账号信息
-}
